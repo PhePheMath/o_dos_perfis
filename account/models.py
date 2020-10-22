5abea6ded3cc79d6347ac1f43a1e95440607fd6e
PhePheMath <PhelyppeMath@outlook.com>
2020-10-21 21:27:21 -0300

    Models for Profile and User

--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
+    auth = models.BooleanField('User Is Authenticated')
