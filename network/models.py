541316120c0c686cdaca99a59e59320cfe93e9e0
PhePheMath <PhelyppeMath@outlook.com>
2020-10-21 22:22:48 -0300

    Reaction's model

--- network/models.py
+++ network/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Reaction(models.Model):
+    # widget = models.ImageField
+    reaction_type = models.CharField(max_length=20)
+    weight = models.IntegerField()
+
+
